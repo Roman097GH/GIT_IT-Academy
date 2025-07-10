4ed9f7a8b540500247e124fb23c61368174a9ec0
Roman <roma.belozerov.90@mail.ru>
2025-07-10 21:04:31 +0300

    Step_1

---
+++ GIT_IT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIT_IT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+
+    }
+}
